165f5cab928d69c285c71ad0d3c02f352010c270
Camilo Alfonso <6633004+vecope@users.noreply.github.com>
2017-10-19 14:28:33 -0500

    To Do list
    Combines the usage of a new navigation layout with tableviews and permanent storage data.

--- To Do List/FirstViewController.swift
+++ To Do List/FirstViewController.swift
@@ -8,18 +8,64 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
-
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var items:[String] = []
+    
+    @IBOutlet var table: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        let itemsObject = UserDefaults.standard.object(forKey: "items")        
+        
+        if let tempItems = itemsObject as? [String] {
+            
+            items = tempItems
+            
+        }
+        table.reloadData()
+        
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == UITableViewCellEditingStyle.delete{
+            
+            items.remove(at: indexPath.row)
+            UserDefaults.standard.set(items, forKey: "items")
+            table.reloadData()
+        }
+        
+    }
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return items.count
+    }
+    
+    
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        
+        
+        cell.textLabel?.text = items[indexPath.row]
+        
+        return cell
+    }
+    
+    
 }
 
--- To Do List/SecondViewController.swift
+++ To Do List/SecondViewController.swift
@@ -8,8 +8,29 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITextFieldDelegate{
 
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBAction func addTask(_ sender: Any) {
+    
+        let itemsObject = UserDefaults.standard.object(forKey: "items")
+        
+        var items:[String]
+        
+        if let tempItems = itemsObject as? [String] {
+            items = tempItems
+            items.append(textField.text!)
+        }
+        else{
+            items = [textField.text!]
+            
+        }
+        
+        UserDefaults.standard.set(items, forKey: "items")
+        textField.text = ""
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +40,16 @@ class SecondViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
 
 }
 
